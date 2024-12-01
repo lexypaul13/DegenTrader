0f634023a022a09b6c9b9bb8a0671da66e2af9a0
Mayowa Paul <lexypaul13@yahoo.com>
2024-11-30 20:53:54 -0500

    Initial Commit

---
+++ DegenTrader/DegenTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DegenTraderApp.swift
+//  DegenTrader
+//
+//  Created by Alex Paul on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DegenTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
